8ed89330e154bd0b4d2aef1510f3052567d1c53e
yuliyambg <yuliyambg@gmail.com>
2021-05-03 22:53:34 -0400

    complete

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-const Home = () => {
-  return (
-    <div>
-      <h1>Home!</h1>
-    </div>
-  );
-};
-
+import Home from './Home'
+import About from './About'
+import Login from './Login'
+import Navbar from './Navbar'
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 ReactDOM.render(
-  <Home />,
+  (
+    <Router>
+      <div>
+        <Navbar />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/about" component={About} />
+      <Route exact path="/login" component={Login} />
+      </div>
+      </Router>),
   document.getElementById('root')
 );
